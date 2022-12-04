e510256835698f60c0c2e7024d41fb527b84129c
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-12-04 19:10:19 +0200

    Create refs.js

---
+++ src/JavaScript/refs.js
@@ -0,0 +1,39 @@
+export const refs = {
+  body: document.querySelector('body'),
+  input: document.getElementById('input'),
+  searchForm: document.getElementById('search-form'),
+  filmCards: document.querySelector('.js-film-cards'),
+  message: document.querySelector('.message'),
+
+  searchBtn: document.querySelector('.js-search-btn'),
+
+  header: document.getElementById('header'),
+  homeLink: document.querySelector('.js-link-home'),
+  headerInput: document.querySelector('.js-input-wrapper'),
+  libraryLink: document.querySelector('.js-link-library'),
+  headerBtn: document.querySelector('.js-button-wrapper'),
+  logoLink: document.querySelector('.js-logo-link'),
+  headerQueueBtn: document.querySelector('.js-header-queue-btn'),
+  headerWatchedBtn: document.querySelector('.js-header-watched-btn'),
+
+  backdrop: document.querySelector('.js-modal-overlay'),
+  modal: document.querySelector('.js-modal'),
+  closeBtn: document.querySelector('.js-close-modal'),
+  main: document.querySelector('.wrapper'),
+
+  homePageContainer: document.querySelector('.js-home-page-container'),
+  libraryPageContainer: document.querySelector('.js-library-container'),
+  libraryFilmCards: document.querySelector('.js-library-film-cards'),
+
+  scrollBtn: document.querySelector('.js-scroll-btn'),
+  preloadWrap: document.querySelector('.preload'),
+
+  paginationContainer: document.querySelector('.js-pagination'),
+
+  footerLightbox: document.querySelector('.js-footer-light-box'),
+  footerLightboxOverlay: document.querySelector('.js-footer-light-box-overlay'),
+  footerLightboxWindow: document.querySelector('.js-footer-lightbox-window'),
+  footerLightboxOpenBtn: document.querySelector('.modal-open-btn'),
+  footerLightboxCloseBtn: document.querySelector('.footer-lightbox-close-btn'),
+  teamList: document.querySelector('.js-team-list'),
+};
