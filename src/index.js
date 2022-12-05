9ca5acb7f46c4f7f53db8b9f4a5af8b1e975cfef
yuri.karmaz <106161829+yuri69k@users.noreply.github.com>
2022-12-05 20:14:40 +0200

    team card

--- src/index.js
+++ src/index.js
@@ -6,3 +6,7 @@ import './JavaScript/localStorageUtil';
 import './JavaScript/modalMarkup';
 import './JavaScript/openModalMovie';
 import './JavaScript/back-to-top';
+import './JavaScript/render_footer_modal';
+import './JavaScript/modal_footer';
+import './JavaScript/slider_glide';
+import './JavaScript/users';
