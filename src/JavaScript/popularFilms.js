80898b1378993bbc86317d3fd32ce53a16de7644
nirqqen97 <93587073+nirqqen97@users.noreply.github.com>
2022-12-05 18:14:10 +0200

    Update popularFilms.js

--- src/JavaScript/popularFilms.js
+++ src/JavaScript/popularFilms.js
@@ -12,7 +12,7 @@ export async function fetchFilmotekaPopularFilms() {
         film
           .map(
             item =>
-              `<div class="photo-card">
+              `<div class="photo-card data-id="${id}"">
           <a class="photo-card__link"  href="https://image.tmdb.org/t/p/w500${item.backdrop_path}">
             <img  src="https://image.tmdb.org/t/p/w500${item.poster_path}" data-source="${item.poster_path}" alt="${item.original_title}" loading="lazy" width="100%" height="90%" style="border-radius: 5px;"/>
           </a>  
