ae85a5acdd4cd3d605c8a9d32bfb1636674c9853
Dima Samoylik <samoylikd@gmail.com>
2022-12-06 01:49:38 +0200

    ///

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import './JavaScript/refs.js';
 import './JavaScript/loader';
 import './JavaScript/localStorageUtil';
 import './JavaScript/modalMarkup';
-import  "./JavaScript/mainMarkUp";
+import './JavaScript/mainMarkUp';
 import './JavaScript/openModalMovie';
 import './JavaScript/back-to-top';
-
