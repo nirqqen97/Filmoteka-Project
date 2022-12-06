5f84b5caa41dd546186089823d79a8745b2ec8ca
Dima Samoylik <samoylikd@gmail.com>
2022-12-06 22:36:28 +0200

    fix style modal movie
    fix style modal movie

--- src/JavaScript/openModalMovie.js
+++ src/JavaScript/openModalMovie.js
@@ -1,47 +1,29 @@
 import { refs } from './refs';
 import axios from 'axios';
-import {createMarkupModal} from './modalMarkup';
+import { createMarkupModal } from './modalMarkup';
 import NewApiService from './apiClass';
-const filmoteka = document.querySelector('.filmoteka');
-
-const newApiService = new NewApiService();
-
-// async function getMovieById(id) {
-//   return await axios.get(
-//     `https://api.themoviedb.org/3/movie/${id}?api_key=8a95c8805d5f43b82cb5bfd70a3069b5&language=en-US`
-//   );
-// }
 
 async function getMovieById(id) {
   try {
     const response = await axios.get(
       `https://api.themoviedb.org/3/movie/${id}?api_key=8a95c8805d5f43b82cb5bfd70a3069b5&language=en-US`
     );
-    const data = createMarkupModal(response.data)   
+    const data = createMarkupModal(response.data);
     refs.backdrop.innerHTML = data;
-   
   } catch (error) {
     console.error(error);
   }
 }
 
 refs.backdrop.addEventListener('click', closeModal);
-filmoteka.addEventListener('click', handleOpenModal);
+refs.filmoteka.addEventListener('click', handleOpenModal);
 
 async function handleOpenModal(event) {
   event.preventDefault();
-  // if (
-  //   !event.target.parentNode.classList.contains('filmoteka') &&
-  //   !event.target.parentNode.classList.contains('info')
-  // )
-  // {
-  //   return;
-  // }
   refs.backdrop.classList.remove('is-hidden');
-  const movieId = event.target.parentNode.parentNode.dataset.id; //из разметки получить id-фильма
+  const movieId = event.target.parentNode.parentNode.dataset.id;
   console.log(movieId);
-  const { data } = getMovieById(movieId); ////получить данные по id-фильма
-
+  const { data } = getMovieById(movieId);
   // const createModalCard = createMarkupModal(data);
   // refs.backdrop.innerHTML = createModalCard;
 }
